4fca110bcc828a8e9978d782052a376885c31200
Sunil Kumar 8 <sunil.kumar2@publicissapient.com>
2022-06-18 09:36:23 +0100

    pact consumer

---
+++ src/main/java/com/sunil/bainsla/kafkapactconsumer/KafkaPactConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.sunil.bainsla.kafkapactconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPactConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPactConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunil/bainsla/kafkapactconsumer/KafkaPactConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.bainsla.kafkapactconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPactConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
